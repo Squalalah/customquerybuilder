91df9d2a1e2bf4747ae724f198b6ed20bb3cdd77
Anthony Montreuil <amontreuilpro@gmail.com>
2023-04-26 22:10:16 +0200

    refactor: constants select from where, delimiters in query

--- src/Builder/QueryBuilder.php
+++ src/Builder/QueryBuilder.php
@@ -2,42 +2,45 @@
 
 namespace CustomQueryBuilder\Builder;
 
+use CustomQueryBuilder\Exception\QueryParameterCountDontMatchException;
 use tests\MyDB;
 
 class QueryBuilder
 {
     private const DYNAMIC_ARGUMENT_DELIMITER = ':';
+    private const SELECT = "SELECT";
+    private const FROM = "FROM";
+    private const WHERE = "WHERE";
+    private const SELECT_DELIMITER_PARAM = ",";
+    private const ESCAPE_DELIMITER = "'";
 
-    private const ESCAPE_DELIMITER = '\'';
-    private string $query;
-    private string $selectQuery = "SELECT ";
+    private string $selectQuery;
+    private string $fromTableQuery;
 
-    private string $fromTableQuery = "FROM ";
-
-    private string $whereClause = "WHERE ";
+    private string $whereClause;
 
     /** @var array<mixed> $parameters */
     private array $parameters = [];
     public function select(string... $args): self
     {
-        $this->selectQuery .= implode(", ", $args);
-        $this->selectQuery .= ' ';
+        $select = self::SELECT . " " . implode(self::SELECT_DELIMITER_PARAM, $args) . " ";
+        $this->selectQuery = $select;
 
         return $this;
     }
 
     public function from(string $table): self
     {
-        $this->fromTableQuery .= $table;
-        $this->fromTableQuery .= ' ';
+        $from = self::FROM . " " . $table . " ";
+        $this->fromTableQuery = $from;
 
         return $this;
     }
 
     public function where(string $whereClause): self
     {
-        $this->whereClause .= $whereClause;
-        $this->whereClause .= ' ';
+        $where = self::WHERE . " " . $whereClause;
+        $this->whereClause = $where;
 
         return $this;
     }
@@ -51,17 +54,22 @@ class QueryBuilder
 
     public function buildQuery(): string
     {
-        $this->query = $this->selectQuery . $this->fromTableQuery;
+        $query = "";
+        if (isset($this->selectQuery)) {
+            $query .= $this->selectQuery;
+        }
+        if (isset($this->fromTableQuery)) {
+            $query .= $this->fromTableQuery;
+        }
+
         if (isset($this->whereClause)) {
+            $whereQuery = $this->whereClause;
             if(!$this->checkParametersCountMatchesWithWhereParams()) {
-                throw new \Exception("The QueryBuilder parameters count does not match the number of dynamic parameter in WHERE clause");
+                throw new QueryParameterCountDontMatchException();
             }
-            $numDynamicParametersInQuery = substr_count($this->whereClause, self::DYNAMIC_ARGUMENT_DELIMITER, 0);
-            if($numDynamicParametersInQuery > 0) {
-                /**
-                 * @var string $parameterName
-                 * @var string $parameterValue
-                 */
+
+            if($this->countParametersInQuery() > 0) {
+                /** @var string $parameterValue */
                 foreach($this->parameters as $parameterName => $parameterValue) {
                     $parsedParameterName = $this->parseParameterName($parameterName);
                     $parsedParameterValue = $this->parseParameterValue($parameterValue);
@@ -69,13 +77,13 @@ class QueryBuilder
                     if(false === $this->isGivenParameterInQuery($parameterName)) {
                         continue;
                     }
-                    $this->whereClause = $this->putParameterInQuery($parsedParameterName, $parsedParameterValue);
+                    $whereQuery = $this->putParameterInQuery($parsedParameterName, $parsedParameterValue, $whereQuery);
                 }
             }
-            $this->query .= trim($this->whereClause);
+            $query .= $whereQuery;
         }
 
-        return $this->query;
+        return $query;
     }
     public function __toString(): string
     {
@@ -100,9 +108,14 @@ class QueryBuilder
         return strpos($this->whereClause, $parameter);
     }
 
-    private function putParameterInQuery(string $parameterName, string $parameterValue): string
+    private function putParameterInQuery(string $parameterName, string $parameterValue, string $query): string
     {
-        return str_replace($parameterName, $parameterValue, $this->whereClause);
+        return str_replace($parameterName, $parameterValue, $query);
+    }
+
+    private function countParametersInQuery(): int
+    {
+        return substr_count($this->whereClause, self::DYNAMIC_ARGUMENT_DELIMITER);
     }
 
     private function checkParametersCountMatchesWithWhereParams(): bool
